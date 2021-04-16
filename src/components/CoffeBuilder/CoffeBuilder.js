a199cd6f767d015717af0e4383245bc5390cf3b4
Your Name <you@example.com>
2021-04-16 15:47:47 +0600

    fixed coffe builder

--- src/components/CoffeBuilder/CoffeBuilder.js
+++ src/components/CoffeBuilder/CoffeBuilder.js
@@ -1,30 +1,30 @@
 import classes from "./CoffeBuilder.module.css";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import CoffeContrlos from "./CoffeContrlos/CoffeContrlos";
 import CoffePreview from "./CpffePreview/CoffePreview";
-// import axios from "axios";
+import axios from "axios";
 
 const CoffeBuilder = () => {
-    // const [ingredients, setIngredients] = useState({});
-    // const [price, setPrice] = useState(0)
-    // const prices = {
-    //     americano: 50,
-    //     cappuccino: 70,
-    //     latte: 70,
-    //     expresso: 80,
-    //     coldcofee: 50,
-    // }
-    // useEffect (() => {
-    //     axios.get("https://builder-57473-default-rtdb.firebaseio.com/default.json")
-    //     .then((response) => {
-    //         setIngredients(response.data.ingredients);
-    //         setPrice(response.data.price)
-    //     })
-    // }, [])
+    const [ingredients, setIngredients] = useState({});
+    const [price, setPrice] = useState(0)
+    const prices = {
+        americano: 50,
+        cappuccino: 70,
+        latte: 70,
+        expresso: 80,
+        coldcofee: 50,
+    }
+    useEffect (() => {
+        axios.get("https://builder-57473-default-rtdb.firebaseio.com/default.json")
+        .then((response) => {
+            setIngredients(response.data.ingredients);
+            setPrice(response.data.price)
+        })
+    }, [])
     return ( 
     <div className={classes.CoffeBuilder}>
-      <CoffePreview />
-        <CoffeContrlos />
+      <CoffePreview ingredients={ingredients}/>
+        <CoffeContrlos ingredients={ingredients} />
     </div>
      );
 }
