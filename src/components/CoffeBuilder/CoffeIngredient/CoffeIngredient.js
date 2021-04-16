6aeff128010dd4a96c15e70e7429156508c4bd57
Your Name <you@example.com>
2021-04-16 15:03:19 +0600

    deleted photos

--- src/components/CoffeBuilder/CoffeIngredient/CoffeIngredient.js
+++ src/components/CoffeBuilder/CoffeIngredient/CoffeIngredient.js
@@ -4,13 +4,6 @@ import americano from "../../../images/americano.svg";
 
 const CoffeIngredient = ({type}) => {
     const types = {
-        americano: {backgroundImage: `url40(${americano})`, width: "50px", height: "50px"},
-        americano: {backgroundImage: `url40(${americano})`, width: "50px", height: "50px"},
-        americano: {backgroundImage: `url40(${americano})`, width: "50px", height: "50px"},
-        americano: {backgroundImage: `url40(${americano})`, width: "50px", height: "50px"},
-        americano: {backgroundImage: `url40(${americano})`, width: "50px", height: "50px"},
-        americano: {backgroundImage: `url40(${americano})`, width: "50px", height: "50px"}
-
     }
     return ( 
     <div className={classes.CoffeIngredient} style={types[type]}></div>
