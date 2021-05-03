7cca7b6708dfd31db0dc304cf11186c9f38197b3
Your Name <you@example.com>
2021-05-03 14:32:51 +0600

    add button classname classes green

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,14 @@
 import classes from "./Button.module.css";
 
-const Button = () => {
-    return ( 
-    <div className={classes.Button}></div>
-     );
+const Button = (props) => {
+    const className = [classes.Button];
+    if (props.green) {
+      className.push(classes.green);
+    }
+    return (
+        <div>
+            <button {...props} className={className.join(' ')}>{props.children}</button>
+        </div>
+    )
 }
- 
 export default Button;
