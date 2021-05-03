d24fe137bf5098aca9756f48ac75c9b10988dee1
Your Name <you@example.com>
2021-05-03 14:35:02 +0600

    added americano and cappuchino svg images

--- src/components/CoffeBuilder/CoffeIngredient/CoffeIngredient.js
+++ src/components/CoffeBuilder/CoffeIngredient/CoffeIngredient.js
@@ -1,9 +1,13 @@
 import classes from "./CoffeIngredient.module.css";
-
+import americano from '../../../images/stur.png';
+import cappuccino from '../../../images/logo.png'
 // import americano from "../../../images/americano.svg";
 
 const CoffeIngredient = ({type}) => {
     const types = {
+        americano:  { backgroundImage: `url(${americano})`,backgroundSize : "cover" , width: "50px", height: "50px"  },
+        cappuccino: { backgroundImage: `url(${cappuccino})` ,backgroundSize : "cover", width: "50px", height: "50px" },   
+       
     }
     return ( 
     <div className={classes.CoffeIngredient} style={types[type]}></div>
