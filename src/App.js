387bca6e8ba45c6cb3ecd6abd7d27a6120a2be06
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-07 15:33:18 +0600

    added Route in App js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import {  Route, Switch } from 'react-router';
 import './App.css';
+import Checkout from './components/Checkout/Checkout';
 import CoffeBuilder from './components/CoffeBuilder/CoffeBuilder';
 import Layout from './components/Layout/Layout';
+import Oreders from './components/Orders/Orders';
+
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <h1>Coffe Builder by Kutman Batyrkanov</h1>
-        <CoffeBuilder />
+        {/* <CoffeBuilder /> */}
+        <Switch>
+          <Route path="/" component={CoffeBuilder} exact />
+          <Route path="./checkout" component={Checkout}/>
+          <Route path="./oreders" component={Oreders}/>
+          {/* <Redirect to='/' /> */}
+        </Switch>
         </Layout>
       
     </div>
