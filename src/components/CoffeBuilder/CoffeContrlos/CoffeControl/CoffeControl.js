34ad054fac2b2b934501de269195b1b21a202271
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-10 14:12:20 +0600

    added dispatch add type and fixed

--- src/components/CoffeBuilder/CoffeContrlos/CoffeControl/CoffeControl.js
+++ src/components/CoffeBuilder/CoffeContrlos/CoffeControl/CoffeControl.js
@@ -1,13 +1,18 @@
+import { useDispatch } from "react-redux";
+import { add, remove } from "../../../../store/actions/builder";
 import Button from "../../../UI/Button/Button";
 import CoffeIngredient from "../../CoffeIngredient/CoffeIngredient";
 import classes from "./CoffeControl.module.css";
 
-const CoffeControl = ({ type, addedIngredients, removeIngredints  }) => {
+const CoffeControl = ({ type, count }) => {
+    const dispatch = useDispatch();
     return ( 
     <div className={classes.CoffeControl}>
-        <Button onClick={addedIngredients}>+</Button>
-        <div className={classes.ingredient} > <CoffeIngredient type={type}/></div>
-        <Button onClick={removeIngredints}>-</Button>
+        <Button onClick={() => dispatch(add(type))}>+</Button>
+        <div className={classes.ingredient} > 
+        <CoffeIngredient type={type} fixed />
+        </div>
+        <Button onClick={() => dispatch(remove(type))} disabled={!count}>-</Button>
     </div>
      );
 }
