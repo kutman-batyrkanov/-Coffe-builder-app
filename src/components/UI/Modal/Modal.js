072ebb148110122a6d4b4c80cbb83122c5dea559
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-10 14:20:18 +0600

    added Backdrop in component Midal js

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,9 +1,18 @@
-import classes from "./Modal.module.css";
+import classes from "./Modal.module.css"
+import Backdrop from "../Backdrop/Backdrop"
 
-const Modal = () => {
-    return ( 
-    <div className={classes.Modal}></div>
-     );
+
+const Modal = ({ show, cancel, children }) => {
+    const style = {
+        transform: show ? "translateY(0)" : "translateY(-100vh)"
+    }
+    return (
+        <div className={classes.Modal}>
+            <Backdrop show={show} click={cancel} />
+            <div className={classes.content} style={style}>
+                {children}
+            </div>
+        </div>
+    )
 }
- 
 export default Modal;
