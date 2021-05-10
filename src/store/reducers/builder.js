389be7763b3705565321c719e35b42fafccce902
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-10 14:25:03 +0600

    creat component builder js in compoenent actions

---
+++ src/store/reducers/builder.js
@@ -0,0 +1,39 @@
+import { ADD_INGREDIENT, REMOVE_INGREDIENT, SET_INGREDIENTS } from "../actions/types";
+
+const initialState = {
+  ingredients: {
+    americano: 1,
+    cappuccino: 1,
+  },
+  price: 200,
+};
+const prices = {
+  americano: 50,
+  cappuccino: 70,
+//   latte: 70,
+//   expresso: 80,
+//   coldcofee: 50,
+};
+
+const builder = (state = initialState, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case ADD_INGREDIENT:
+      newState.ingredients[action.ingredient]++;
+      newState.price += prices[action.ingredient];
+      break;
+    case REMOVE_INGREDIENT:
+      newState.ingredients[action.ingredient]--;
+      newState.price -= prices[action.ingredient];
+      break;
+    case SET_INGREDIENTS:
+        newState.ingredients = action.data.ingredients;
+        newState.price = action.data.price;
+        break;
+      default:
+          break;
+  }
+  return newState;
+};
+
+export default builder;
