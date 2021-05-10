9a0cc98606fbf3d4fddc2d8971484003952555a4
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-10 14:15:21 +0600

    deleted images logo and added images logo1

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,10 @@
 import classes from "./Logo.module.css";
-// import logo from "../../images/logo.png";
+import logo from "../../images/logo1.png";
 
 const Logo = () => {
     return ( 
     <div className={classes.Logo}>
-        {/* <img src={logo} alt={"starbucks"} /> */}
+        <img src={logo} alt={"starbucks"} />
         <div>Coffe</div>
     </div>
      );
