1d554cb19695301d2daa8a55a2a9d5e44a69c262
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-10 15:19:04 +0600

    formated cod

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import {  Route, Switch } from 'react-router';
-import './App.css';
-import Checkout from './components/Checkout/Checkout';
-import CoffeBuilder from './components/CoffeBuilder/CoffeBuilder';
-import Layout from './components/Layout/Layout';
-import Oreders from './components/Orders/Orders';
-
+import { Route, Switch } from "react-router";
+import "./App.css";
+import Checkout from "./components/Checkout/Checkout";
+import CoffeBuilder from "./components/CoffeBuilder/CoffeBuilder";
+import Layout from "./components/Layout/Layout";
+import Oreders from "./components/Orders/Orders";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <h1>Coffe Builder by Kutman Batyrkanov</h1>
-        {/* <CoffeBuilder /> */}
         <Switch>
           <Route path="/" component={CoffeBuilder} exact />
-          <Route path="./checkout" component={Checkout}/>
-          <Route path="./oreders" component={Oreders}/>
-          {/* <Redirect to='/' /> */}
+          <Route path="./checkout" component={Checkout} />
+          <Route path="./oreders" component={Oreders} />
         </Switch>
-        </Layout>
-      
+      </Layout>
     </div>
   );
 }
