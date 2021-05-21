a0dd7c78f96424a8dafe74d162f6ac33a1f78a5d
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-21 12:59:20 +0600

    create component loading

---
+++ src/Loading/Loading.js
@@ -0,0 +1,11 @@
+import classes from "./Loading.module.css";
+
+const Loading = () => {
+    return ( 
+    <div className={classes.Loading}>
+        <div className={classes.loadingg}></div>
+    </div>
+     );
+}
+ 
+export default Loading;
