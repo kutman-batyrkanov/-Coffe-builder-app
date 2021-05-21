ca186835e5a454b79b0679f13238d9d7d3d65f5a
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-21 12:58:02 +0600

    formated cod in order js

--- src/components/Orders/Order/Order.js
+++ src/components/Orders/Order/Order.js
@@ -1,16 +1,23 @@
 import classes from "./Order.module.css";
 
 const Order = ({ name, phone, address, ingredients, price }) => {
-  const outputIngredients = Object.keys(ingredients)
-    .map(ingredient => <em>{ingredient} - {ingredients[ingredient]}</em>);
+  const outputIngredients = Object.keys(ingredients).map((ingredient) => (
+    <em key={ingredient}>
+      {ingredient} - {ingredients[ingredient]}
+    </em>
+  ));
 
   return (
     <div className={classes.Order}>
-      <div>{name}, {phone}, {address}</div>
+      <div className={classes.test}>
+        <h1>{name}</h1>
+        <span>{phone}</span>
+        <h5>{address}</h5>
+      </div>
       <div>{outputIngredients}</div>
-      <strong>{price}</strong>
+      <strong>{price}som</strong>
     </div>
   );
-}
+};
 
 export default Order;
