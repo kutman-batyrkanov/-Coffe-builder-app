74d343e1eaf704eec5cd354eea6675e60c0e5baf
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-21 12:57:24 +0600

    fixed nav item component

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,10 +1,10 @@
 import classes from "./NavItem.module.css";
 import { NavLink } from "react-router-dom";
 
-const NavItem = ({ url, children }) => {
+const NavItem = ({ url, children, exact, active }) => {
   return (
     <div className={classes.NavItem}>
-      <NavLink to={url} activeClassName={classes.active} exact>
+      <NavLink to={url} activeClassName={classes.active} exact={exact}>
         {children}
       </NavLink>
     </div>
