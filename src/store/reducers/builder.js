2e1b442b655e0351a98765a2c6171bb3b1577622
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-21 13:01:54 +0600

    formated builder component in actions

--- src/store/reducers/builder.js
+++ src/store/reducers/builder.js
@@ -1,44 +1,40 @@
-import {
-  ADD_INGREDIENT,
-  REMOVE_INGREDIENT,
-  SET_INGREDIENTS,
-} from "../actions/types";
+import { ADD_INGREDIENT, REMOVE_INGREDIENT, SET_INGREDIENTS } from "../actions/types";
+
 
 const initialState = {
-  ingredients: {
-    americano: 1,
-    cappuccino: 1,
-    latte: 1,
-    smooth: 1,
-    strcoffe: 1,
-    coldcoffe: 1,
-  },
-  price: 200,
+    ingredients: {
+    },
+    price:0,
 };
 const prices = {
-  americano: 50,
-  cappuccino: 70,
+    americano: 50,
+    cappuccino: 55,
+    coldcoffe: 45,
+    latte: 60,
+    smooth: 65,
+    strcoffe: 70
 };
 
 const builder = (state = initialState, action) => {
-  const newState = { ...state };
-  switch (action.type) {
-    case ADD_INGREDIENT:
-      newState.ingredients[action.ingredient]++;
-      newState.price += prices[action.ingredient];
-      break;
-    case REMOVE_INGREDIENT:
-      newState.ingredients[action.ingredient]--;
-      newState.price -= prices[action.ingredient];
-      break;
-    case SET_INGREDIENTS:
-      newState.ingredients = action.data.ingredients;
-      newState.price = action.data.price;
-      break;
-    default:
-      break;
-  }
-  return newState;
-};
+    const newState = { ...state };
+
+    switch (action.type) {
+        case ADD_INGREDIENT:
+            newState.ingredients[action.ingredient]++;
+            newState.price += prices[action.ingredient];
+            break;
+        case REMOVE_INGREDIENT:
+            newState.ingredients[action.ingredient]--;
+            newState.price -= prices[action.ingredient];
+            break;
+        case SET_INGREDIENTS:
+            return { ...action.data };
+
+        default:
+            break;
+    }
+
+    return newState;
+}
 
 export default builder;
