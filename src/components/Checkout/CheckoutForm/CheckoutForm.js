f6121c75a0dc9477868678cd2e156891fd67c13f
KUTMAN_BATYRKANOV <KUTMAN_BATYRKANOV@example.com>
2021-05-28 15:30:02 +0600

    added classes name address ans phone

--- src/components/Checkout/CheckoutForm/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm/CheckoutForm.js
@@ -4,20 +4,22 @@ import classes from "./CheckoutForm.module.css"
 const CheckoutForm = ({ cancelCallback, submitCallback }) => {
   return (
     <form className={classes.CheckoutForm} onSubmit={submitCallback}>
-      <div>
+      <div className={classes.name}>
         <label htmlFor="name">Name</label>
         <input type="text" name="name" id="name" required />
       </div>
-      <div>
+      <div className={classes.address}>
         <label htmlFor="address">Address</label>
         <input type="text" name="address" id="address" required />
       </div>
-      <div>
+      <div className={classes.phone}>
         <label htmlFor="phone">Phone</label>
         <input type="text" name="phone" id="phone" required pattern="0[0-9]{9}" />
       </div>
+      <div className={classes.Button}>
       <Button green="green">Checkout</Button>
-      <Button onClick={cancelCallback}>Cancel</Button>
+      <Button onClick={cancelCallback} red="red">Cancel</Button>
+      </div>
     </form>
   );
 }
